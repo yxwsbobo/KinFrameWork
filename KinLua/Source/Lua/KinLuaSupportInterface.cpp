5eb2af51dfd2ddf91e9316f8c65ff25d2f58431a
yxwsbobo <yxwsbobo@gmail.com>
2018-09-25 08:13:43 +0800

    mark from home

---
+++ KinLua/Source/Lua/KinLuaSupportInterface.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Kin on 2018/9/24 0024.
+// Copyright © 2018 jihuisoft. All rights reserved.
+//
+
+#include "KinLuaSupportInterface.h"
---
+++ KinLua/Source/Lua/KinLuaSupportInterface.h
@@ -0,0 +1,28 @@
+//
+// Created by Kin on 2018/9/24 0024.
+// Copyright © 2018 jihuisoft. All rights reserved.
+//
+
+#ifndef KINLUA_KINLUASUPPORTINTERFACE_H
+#define KINLUA_KINLUASUPPORTINTERFACE_H
+
+namespace KinLua
+{
+
+/**
+ * @brief
+ * @date 2018/9/24 0024
+ * @author Kin
+ * @copyright Copyright © 2018 jihuisoft. All rights reserved.
+ */
+    class KinLuaSupportInterface
+    {
+    public:
+        KinLuaSupportInterface() = default;
+
+        ~KinLuaSupportInterface() = default;
+
+    };
+
+}
+#endif //KINLUA_KINLUASUPPORTINTERFACE_H
