ac44aee2fa60f555fa722daf56de90aba79669b9
yxwsbobo <yxwsbobo@gmail.com>
2018-11-03 18:20:54 +0800

    AddSome TempTag

---
+++ KinBase/Source/KinBase/KinFileSystem.h
@@ -0,0 +1,28 @@
+//
+// Created by Kin on 2018-11-03.
+// Copyright © 2018 jihuisoft. All rights reserved.
+//
+
+#ifndef KINBASE_KINFILESYSTEM_H
+#define KINBASE_KINFILESYSTEM_H
+
+namespace KinBase
+{
+
+    /**
+     * @brief
+     * @date 2018-11-03
+     * @author Kin
+     * @copyright Copyright © 2018 jihuisoft. All rights reserved.
+     */
+    class KinFileSystem
+    {
+    public:
+        KinFileSystem() = default;
+
+        ~KinFileSystem() = default;
+
+    };
+
+}
+#endif //KINBASE_KINFILESYSTEM_H
---
+++ KinBase/Source/KinBase/KinFileSystem.hpp
@@ -0,0 +1,9 @@
+//
+// Created by Kin on 2018-11-03.
+// Copyright © 2018 jihuisoft. All rights reserved.
+//
+
+#ifndef KINBASE_KINFILESYSTEM_HPP
+#define KINBASE_KINFILESYSTEM_HPP
+
+#endif //KINBASE_KINFILESYSTEM_HPP
